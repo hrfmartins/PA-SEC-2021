be0faa75ef146a6b4747bdf043a166dff4b484db
Hugo <hugo.martins365@gmail.com>
2021-07-13 22:54:17 +0100

    Utils used throughout the project

---
+++ src/utils.jl
@@ -0,0 +1,55 @@
+
+function second_operand(exp)
+    return exp.args[3]
+end
+
+function first_operand(exp)
+    return exp.args[2]
+end
+
+function isAddition(exp)
+    return exp.args[1] == :+
+end
+
+function isSubtraction(exp)
+    return exp.args[1] == :-
+end
+
+function isMultiplication(exp)
+    return exp.args[1] == :*
+end
+
+function isDivision(exp)
+    return exp.args[1] == :/
+end
+
+function isRationalDivision(exp)
+    return exp.args[1] == ://
+end
+
+
+function isRemainder(exp)
+    return exp.args[1] == :%
+end
+
+function isBiggerOp(exp)
+    return exp.args[1] == :>
+end
+
+function isSmallerOp(exp)
+    return exp.args[1] == :<
+end
+
+function isSmallerOrEqualOp(exp)
+    return exp.args[1] == :<=
+end
+
+function isBiggerOrEqualOp(exp)
+    return exp.args[1] == :>=
+end
+
+function isEqual(exp)
+    return exp.args[1] == :(==)
+end
+
+macro evaluateSecond(exp, env) return :(evaluate(second_operand($exp), $env)) end
