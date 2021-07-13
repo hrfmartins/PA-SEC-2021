7ece4730b769b64c4a92559a4bc8e8889c46d36d
Hugo <hugo.martins365@gmail.com>
2021-07-13 22:53:45 +0100

    Base Eval with operations

---
+++ src/eval.jl
@@ -0,0 +1,55 @@
+include("utils.jl")
+
+function evaluate(exp, env)
+    if (typeof(exp) == Int64)
+        return exp
+    end
+
+    if (typeof(exp) == String)
+        return exp
+    end
+
+    if (typeof(exp) == Expr)
+        if isAddition(exp)
+            evaluate(first_operand(exp), env) + evaluate(second_operand(exp), env)
+
+        elseif isSubtraction(exp)
+            evaluate(first_operand(exp), env) - evaluate(second_operand(exp), env)
+
+        elseif isMultiplication(exp)
+            evaluate(first_operand(exp), env) * evaluate(second_operand(exp), env)
+
+        elseif isRemainder(exp)
+            evaluate(first_operand(exp), env) % evaluate(second_operand(exp), env)
+
+        elseif isDivision(exp)
+            evaluate(first_operand(exp), env) / evaluate(second_operand(exp), env)
+
+        elseif isRationalDivision(exp)
+            evaluate(first_operand(exp), env) // evaluate(second_operand(exp), env)
+
+        elseif isEqual(exp)
+            evaluate(first_operand(exp), env) == evaluate(second_operand(exp), env)
+
+        elseif isSmallerOp(exp)
+            evaluate(first_operand(exp), env) < evaluate(second_operand(exp), env)
+
+        elseif isBiggerOp(exp)
+            evaluate(first_operand(exp), env) > evaluate(second_operand(exp), env)
+        
+        elseif isSmallerOrEqualOp(exp)
+            evaluate(first_operand(exp), env) <= evaluate(second_operand(exp), env)
+
+        elseif isBiggerOrEqualOp(exp)
+            evaluate(first_operand(exp), env) >= evaluate(second_operand(exp), env)
+
+        else
+            error("Unknown Expression ",exp," Type")
+        end
+    
+    else
+        error("Unknown ", exp, " Type")
+    end
+end
+
+@ifeval isBiggerOrEqualOp(exp)
